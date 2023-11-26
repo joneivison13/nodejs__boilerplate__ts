d57e67329db5f043c9ac4fbf43b39703e8bac360
joneivison13 <joneivison356@outlook.com>
2023-11-26 14:14:14 -0300

    adiciona pid e NODE_ENV ao log de execução do backend

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { app } from "./app";
 import logger from "./utils/logger";
 
 const PORT = process.env.PORT || 3333;
+
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log(
+    `Server running on port ${PORT}, ${process.env.NODE_ENV} mode, ${process.pid} process id`
+  );
 });
